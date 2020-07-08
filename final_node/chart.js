2791a70dacbe62904e2444376c11605749b9f4a7
kimgeonuk <67300454+kimgeonuk@users.noreply.github.com>
2020-07-08 22:19:21 +0900

    Create chart.js

---
+++ final_node/chart.js
@@ -0,0 +1,40 @@
+ <canvas id="myChart" width="1200" height="400"></canvas>
+
+<script>
+    console.log('{{data.regDate|safe}}')
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var data = {
+        type: 'line',
+        data: {
+            labels: {{time|safe}},
+            datasets: [{
+                label: 'Humi',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                fill : false,
+                lineTension:0.1,
+                data: {{humi}},
+            },
+            {
+                label: "Temp",
+                backgroundColor: 'rgb(75, 99, 132)',
+                borderColor: 'rgb(75, 99, 132)',
+                fill : false,
+                lineTension:0.1,
+                data: {{temp}},
+            }]
+        },
+        options: {
+            responsive: false,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                        fontSize : 14,
+                    }
+                }]
+            }
+        }
+    }
+    var chart = new Chart(ctx, data);
+</script>
